edbafd0ff02ee43131792cb399c2e7b5eedcc560
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2019-02-20 18:21:44 +0100

    Add files via upload

---
+++ Lezione2/Anagramma.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_STRLEN 256
+
+// calcola la lunghezza di una stringa
+
+int strLen (char *s){
+  int len=0;
+  while (*(s+len) != 0 && len < MAX_STRLEN){
+    len++;}
+  return len;
+}
+
+// stampa una stringa inserita
+
+void printStr (char *s){
+  int i=0;
+  while (*(s+i) != 0 && i<MAX_STRLEN){
+    printf("%c",*(s+i));
+    i++;}
+  printf("\n");
+}
+
+int member (char *s, char ch){
+  int i=0;
+  int found=0;
+  while (*(s+i) != 0 && i<MAX_STRLEN && !found){
+    if (*(s+i)==ch)
+      found=1;
+    else i++;}
+  return found;
+}
+
+int anagramma (unsigned char *s1,unsigned char *s2){
+  int ok=1;
+  int i=0;
+  if (strLen(s1)==strLen(s2)){      //se la lunghezza delle stringhe è diversa torna subito FALSE
+    while (*(s1+i) != 0 && i<MAX_STRLEN && ok){
+      if (member(s2,*(s1+i)))
+        i++;
+      else
+        ok=0;}}
+  else
+    return 0;
+  return ok;
+}
+
+int main(){
+  char string1[MAX_STRLEN];
+  char string2[MAX_STRLEN];
+  unsigned char *s1=string1;
+  unsigned char *s2=string2;
+  scanf("%s",string1);
+  scanf("%s",string2);
+  printf("%d\n",anagramma(s1,s2));
+  return 0;
+}
