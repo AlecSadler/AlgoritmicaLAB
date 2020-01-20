18ab39c559b374ec87d490ad1170e82f2a55ab95
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2020-01-20 11:37:46 +0100

    Update Ricerca.c

--- Lezione9/Ricerca.c
+++ Lezione9/Ricerca.c
@@ -4,89 +4,59 @@
 struct btree{
   int key;
   struct btree* left;
-  struct btree* right;};
+  struct btree* right;
+};
 
-struct src_results{
-  int val;                     // uso una lista per memorizzare gli esiti delle ricerche
-  struct src_results* next;};
+typedef struct btree tree;
 
-typedef struct btree btree;
-typedef struct src_results src_results;
-
-void push_tail(src_results **head,int el){
-  src_results *new=malloc(sizeof(src_results));
-  new->val=el;
-  new->next=NULL;
-  if (*head==NULL){
-    *head=new;}
-  else{
-    src_results *cur=*head;
-    while (cur->next!=NULL){
-      cur=cur->next;}
-    cur->next=new;}
+void insert (tree **t,int n){
+    if (*t==NULL){
+    tree *new= malloc(sizeof(tree));
+    new->key= n;
+    new->left=NULL;
+    new->right=NULL;
+    *t=new;
+    return;
+  }
+  if (n <= (*t)->key)
+    insert(&(*t)->left,n);
+  else
+    insert (&(*t)->right,n);
 }
 
-void insert (btree** t,int el){
-  btree *new=malloc(sizeof(btree));
-  new->left=NULL;
-  new->right=NULL;
-  new->key=el;
-  if (*t==NULL){
-    *t=new;}
-  else{
-    btree *cur=*t;
-    btree *par;
-    while (cur!=NULL){
-      par=cur;
-      if (cur->key < el)
-        cur=cur->right;
-      else
-        cur=cur->left;}
-    if (par->key < el)
-      par->right=new;
-    else
-      par->left=new;}
-}
-
-int search_key (btree *t,int el){
+int binarySearch (tree *t,int x){
   int depth=0;
-  btree *cur=t;
-  while (cur!=NULL){
-    if (cur->key==el)
+  tree *cur=t;
+  while (cur!=NULL) {
+    if (cur->key==x)
       return depth;
-    else if (cur->key < el)
-      cur=cur->right;
-    else
+    else if (x <= cur->key)
       cur=cur->left;
-    depth++;}
+    else
+      cur= cur->right;
+    depth++;
+  }
   return -1;
 }
 
-void printList (src_results *head){
-  while (head!=NULL){
-    if (head->val!=-1)
-      printf("%d\n",head->val);
-    else
-      printf("NO\n");
-    head=head->next;}
+void main(){
+  int elts, n, i=0, end=0;
+  tree *myT=NULL;
+  scanf("%d",&elts);
+  while (i<elts) {
+    scanf("%d",&n);
+    insert(&(myT),n);
+    i++;
+  }
+  while (!end) {
+    scanf("%d",&n);
+    if (n<0)
+      end++;
+    else{
+      int res = binarySearch(myT,n);
+      if (res==-1) printf("NO\n");
+      else printf("%d\n",res);
+    }
+  }
 }
 
-int main(){
-  int end=0;
-  int el,n,i,src;
-  src_results *res=NULL;
-  btree *t=NULL;
-  scanf("%d",&n);
-  for (i=0;i<n;i++){
-    scanf("%d",&el);
-    insert(&t,el);}
-  while (!end){
-    scanf("%d",&el);
-    if (el>=0){
-      src=search_key(t,el);
-      push_tail(&res,src);}
-    else
-      end++;}
-  printList(res);
-  return 0;
-}
