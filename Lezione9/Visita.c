f467577db8a249119b66905f1246041bf206ccc6
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2020-01-20 17:14:04 +0100

    Update Visita.c

--- Lezione9/Visita.c
+++ Lezione9/Visita.c
@@ -4,52 +4,42 @@
 struct btree{
   int key;
   struct btree* left;
-  struct btree* right;};
+  struct btree* right;
+};
 
-typedef struct btree btree;
+typedef struct btree tree;
 
-void insert (btree** t,int el){
-  btree *new=malloc(sizeof(btree));
-  new->left=NULL;
-  new->right=NULL;
-  new->key=el;
-  if (*t==NULL){
-    *t=new;}
-  else{
-    btree *cur=*t;
-    btree *par;
-    while (cur!=NULL){
-      par=cur;
-      if (cur->key < el)
-        cur=cur->right;
-      else
-        cur=cur->left;}
-    if (par->key < el)
-      par->right=new;
-    else
-      par->left=new;}
-}
-
-int max_depth (btree *t){
-  if (t!=NULL){
-    int rd=max_depth(t->right);
-    int ld=max_depth(t->left);
-    if (rd>ld)
-      return 1+rd;
-    else
-      return 1+ld;}
+void insert (tree **t,int n){
+    if (*t==NULL){
+    tree *new= malloc(sizeof(tree));
+    new->key= n;
+    new->left=NULL;
+    new->right=NULL;
+    *t=new;
+    return;
+  }
+  if (n < (*t)->key)
+    insert(&(*t)->left,n);
   else
-    return 0;
+    insert (&(*t)->right,n);
 }
 
-int main(){
-  int end=0;
-  int el,n,i;
-  btree *t=NULL;
-  scanf("%d",&n);
-  for (i=0;i<n;i++){
-    scanf("%d",&el);
-    insert(&t,el);}
-  printf("%d\n",max_depth(t));
-  return 0;
+int maxDepth(tree *t){
+  if (t==NULL) return 0;
+  int rd= maxDepth(t->right);
+  int ld= maxDepth(t->left);
+  if (rd>ld) return 1+ rd;
+  else return 1+ld;
+}
+
+void main(){
+  int elts, n, i=0;
+  tree *myT=NULL;
+  scanf("%d",&elts);
+  while (i<elts) {
+    scanf("%d",&n);
+    insert(&(myT),n);
+    i++;
+  }
+  printf("%d\n",maxDepth(myT));
 }
