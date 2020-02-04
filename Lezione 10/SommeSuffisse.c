3593b427b0b3d19b52642db02ca95ce1f94b1320
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2020-02-04 19:48:19 +0100

    Update SommeSuffisse.c

--- Lezione 10/SommeSuffisse.c
+++ Lezione 10/SommeSuffisse.c
@@ -1,54 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
-  int key;
-  struct node* next;};
+struct node {
+    int key;
+    struct node* next;
+};
 
 typedef struct node node;
 
-void push_tail(node **head,int el){
+void push(node **head,int el){
   node *new=malloc(sizeof(node));
   new->key=el;
   new->next=NULL;
   if (*head==NULL){
-    *head=new;}
-  else{
-    node *cur=*head;
-    while (cur->next!=NULL){
-      cur=cur->next;}
-    cur->next=new;}
+    *head=new;
+    return;
+  }
+  node *cur= *head;
+  while (cur->next!=NULL) {
+    cur=cur->next;
+  }
+  cur->next=new;
 }
 
-void printList (node *head){
-  while (head!=NULL){
-    printf("%d ",head->key);
-    head=head->next;}
-  printf("\n");
+void suffixSum(node *head, int* s){
+  int f;
+  if(head==NULL) return;
+  *s= *s + head->key;
+  f= *s - head->key;
+  suffixSum(head->next, s);
+  head->key= *s - f - head->key;
 }
 
-int sum_fw (node *head){
-  if (head!=NULL)
-    return head->key + sum_fw(head->next);
-  else return 0;
+void printList(node *head){
+  if (head==NULL) return;
+  printf("%d ",head->key);
+  printList(head->next);
 }
 
-void suffix_sum (node **head){
-  if (*head!=NULL){
-    (*head)->key=sum_fw((*head)->next);
-    suffix_sum(&(*head)->next);}
-  else return;
-}
-
-int main(){
-  int n,i,el;
-  node *list=NULL;
+void main(){
+  int el, n, i, sum=0;
+  node *lst=NULL;
   scanf("%d",&n);
   for (i=0;i<n;i++){
     scanf("%d",&el);
-    push_tail(&list,el);}
-  printList(list);
-  suffix_sum(&list);
-  printList(list);
-  return 0;
+    push(&lst,el);
+  }
+  printList(lst);
+  printf("\n");
+  suffixSum(lst,&sum);
+  printList(lst);
+  printf("\n");
 }
