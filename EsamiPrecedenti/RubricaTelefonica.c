f094fd79d0d525397bfd36aacfdd0c2670b64759
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2020-02-08 15:31:22 +0100

    Update RubricaTelefonica.c

--- EsamiPrecedenti/RubricaTelefonica.c
+++ EsamiPrecedenti/RubricaTelefonica.c
@@ -2,65 +2,74 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXLEN 101
+#define MAXNOME 101
 #define MAXNUM 21
 
-struct node{
-  char* nome;
-  char* numero;
-  struct node *next;};
+struct contatto{
+	char* nome;
+	char* numero;};
+typedef struct contatto contatto;
 
+struct node{
+	contatto record;
+	struct node* next;};
 typedef struct node node;
 
-// inserisce in ordine crescente,per stringhe
-void push_order (node **head,char *nome,char *numero){
-  node *new=malloc(sizeof(node));
-  new->nome=nome;
-  new->numero=numero;
-  if (*head==NULL){
-    *head=new;}
-  else{
-    node *cur=*head;
-    node *prev=NULL;
-    while (cur!=NULL && strcmp(new->nome,cur->nome)>0 ){
-      prev=cur;
-      cur=cur->next;}
-    if (prev==NULL){
-      new->next=*head;
-      *head=new;}
-    else{
-      prev->next=new;
-      new->next=cur;}}
+int fHash (char* str, int n){
+	int i, sum=0;
+	for (i=0;i<strlen(str);i++){
+		sum= (unsigned int)str[i] + sum;
+	}
+	return sum % (2*n);
 }
 
-void hash_insert (node **tab,int n,char *nome,char *numero){
-  int i,sum=0;
-  for (i=0;i<strlen(nome);i++){
-    sum= sum + (unsigned int)nome[i];}
-  int hx = sum % (2*n);
-  push_order(&tab[hx],nome,numero);
+void insert (node **tab, int n, char* name, char* num){
+	contatto new;
+	new.nome=name;
+	new.numero=num;
+	node *newNode= malloc(sizeof(node));
+	newNode->record= new;
+	int pos = fHash(name,n);
+	node *cur= tab[pos];
+	node *prev= NULL;
+	if (tab[pos]==NULL){
+		tab[pos]=newNode;
+		tab[pos]->next=NULL;
+		return;
+	}
+	while (cur!=NULL && strcmp(cur->record.nome,name)<0){
+		prev= cur;
+		cur= cur->next;
+	}
+	if (prev==NULL){
+		newNode->next= tab[pos];
+		tab[pos]=newNode;
+		return;
+	}
+	prev->next=newNode;
+	newNode->next=cur;
 }
 
-void printList (node *head){
-  if (head!=NULL){
-    printf("%s %s\n",head->nome,head->numero);
-    printList(head->next);}
-  else return;
+void printList(node *head){
+	if (head==NULL) return;
+	printf("%s %s\n",head->record.nome,head->record.numero);
+	printList(head->next);
 }
 
-int main(){
-  int n,i,k;
-  scanf("%d",&n);
-  node **tab=malloc(2*n*sizeof(node));
-  for (i=0;i<(2*n);i++){
-    tab[i]=NULL;}
-  for (i=0;i<n;i++){
-    char *nome=malloc(MAXLEN*sizeof(char));
-    char *numero=malloc(MAXNUM*sizeof(char));
-    scanf("%s",nome);
-    scanf("%s",numero);
-    hash_insert(tab,n,nome,numero);}
-  scanf("%d",&k);
-  printList(tab[k]);
-  return 0;
+void main(){
+	int n, i, k;
+	scanf("%d",&n);
+	node **tab= malloc(2*n*sizeof(node));
+	for (i=0;i<(2*n);i++){
+		tab[i]=NULL;
+	}
+	for (i=0;i<n;i++){
+		char* nom= malloc(MAXNOME*sizeof(char));
+		char* num= malloc(MAXNUM*sizeof(char));
+		scanf("%s",nom);
+		scanf("%s",num);
+		insert(tab,n,nom,num);
+	}
+	scanf("%d",&k);
+	printList(tab[k]);
 }
