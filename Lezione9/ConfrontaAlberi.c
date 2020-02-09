bfeff60eff8165d53cc5363aafe042c6920db0a5
Salvo Firera <45435648+AlecSadler@users.noreply.github.com>
2020-02-09 17:23:33 +0100

    Update ConfrontaAlberi.c

--- Lezione9/ConfrontaAlberi.c
+++ Lezione9/ConfrontaAlberi.c
@@ -4,56 +4,47 @@
 struct btree{
   int key;
   struct btree* left;
-  struct btree* right;};
+  struct btree* right;
+};
 
-typedef struct btree btree;
+typedef struct btree tree;
 
-void insert (btree** t,int el){
-  btree *new=malloc(sizeof(btree));
-  new->left=NULL;
-  new->right=NULL;
-  new->key=el;
-  if (*t==NULL){
-    *t=new;}
-  else{
-    btree *cur=*t;
-    btree *par;
-    while (cur!=NULL){
-      par=cur;
-      if (cur->key < el)
-        cur=cur->right;
-      else
-        cur=cur->left;}
-    if (par->key < el)
-      par->right=new;
-    else
-      par->left=new;}
-}
-
-int key_prec (btree *t1,btree *t2,int el){
-  if (t1==NULL || t2==NULL)
-      return 0;
-  else if (t1->key==el && t2->key==el)
-    return 1;
-  else if (t1->key != t2->key)
-    return 0;
-  else if (t1->key < el)
-    return key_prec(t1->right,t2->right,el);
+void insert (tree **t,int n){
+    if (*t==NULL){
+    tree *new= malloc(sizeof(tree));
+    new->key= n;
+    new->left=NULL;
+    new->right=NULL;
+    *t=new;
+    return;
+  }
+  if (n < (*t)->key)
+    insert(&(*t)->left,n);
   else
-    return key_prec(t1->left,t2->left,el);
+    insert (&(*t)->right,n);
 }
 
-int main(){
-  int k,el,n,i;
-  btree *t1=NULL;
-  btree *t2=NULL;
-  scanf("%d %d",&n,&k);
-  for (i=0;i<n;i++){
-    scanf("%d",&el);
-    insert(&t1,el);}
-  for (i=0;i<n;i++){
-    scanf("%d",&el);
-    insert(&t2,el);}
-  printf("%d\n",key_prec(t1,t2,k));
-  return 0;
+int samePath (tree *t1, tree *t2, int k){
+	if (t1->key==k && t2->key==k)
+		return 1;
+	if (k < t1->key && k < t2->key && t1->key == t2->key)
+		return samePath(t1->left,t2->left,k);
+	else if (k > t1->key && k > t2->key && t1->key == t2->key)
+		return samePath(t1->right,t2->right,k);
+	else return 0;
+}
+
+void main(){
+	int i, n, k, el;
+	tree *t1=NULL, *t2=NULL;
+	scanf("%d%d\n",&n,&k);
+	for (i=0;i<n;i++){
+		scanf("%d",&el);
+		insert(&t1,el);
+	}
+	for (i=0;i<n;i++){
+		scanf("%d",&el);
+		insert(&t2,el);
+	}
+	printf("%d\n",samePath(t1,t2,k));
 }
